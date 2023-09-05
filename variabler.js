d1073cce168f77513452fb2defedb329239ef2ad
= <=>
2023-09-05 14:26:28 +0200

    intro

---
+++ variabler.js
@@ -0,0 +1,17 @@
+let name = 'angelica'
+console.log(typeof(name))
+const greeting = `hej ${name}`
+console.log(greeting)
+
+let age = 46
+console.log(typeof (age))
+
+let x = Number.MAX_VALUE
+
+console.log(name+x)
+
+
+let y = x * 2 + 1 
+typeof(y)
+isNaN (7)
+`function console.log(val)}
